7bbc3d76ae9954a0c4a100391901b476813ab49a
Gabi Scardine Silva <gabiscardinesilva@Gabis-MacBook-Pro.local>
2017-01-26 15:07:32 -0500

    Inflate custom view layout programatically

--- app/src/main/java/io/intrepid/contest/customviews/RemovableImageFrameLayout.java
+++ app/src/main/java/io/intrepid/contest/customviews/RemovableImageFrameLayout.java
@@ -2,6 +2,7 @@ package io.intrepid.contest.customviews;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -12,24 +13,38 @@ import io.intrepid.contest.R;
 
 public class RemovableImageFrameLayout extends FrameLayout {
 
+    @BindView(R.id.removable_image_root_view)
+    ViewGroup rootView;
     @BindView(R.id.removable_image_image_view)
     ImageView imageView;
 
     public RemovableImageFrameLayout(final Context context) {
         super(context);
+        init(context);
     }
 
     public RemovableImageFrameLayout(final Context context, final AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public RemovableImageFrameLayout(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        inflate(context, R.layout.removable_image_layout, this);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+
+        if (isInEditMode()) {
+            return;
+        }
+
         ButterKnife.bind(this);
     }
 
