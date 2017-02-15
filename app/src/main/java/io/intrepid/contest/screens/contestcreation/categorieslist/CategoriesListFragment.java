f601826d2f65e8e5a17c79a9b0dc28fae33c0cce
Abass Bayo-Awoyemi <abass.bayo@gmail.com>
2017-02-15 15:01:53 -0500

    Testing jenkins

--- app/src/main/java/io/intrepid/contest/screens/contestcreation/categorieslist/CategoriesListFragment.java
+++ app/src/main/java/io/intrepid/contest/screens/contestcreation/categorieslist/CategoriesListFragment.java
@@ -21,7 +21,7 @@ import io.intrepid.contest.screens.contestcreation.EditContestContract;
 import io.intrepid.contest.utils.dragdrop.SimpleItemTouchHelperCallback;
 
 
-public class CategoriesListFragment extends BaseFragment<CategoriesListPresenter> implements CategoriesContract.View, ContestCreationFragment {
+public class CategoriesListFragment extends BaseFragment<CategoriesListPresenter> implements CategoriesListContract.View, ContestCreationFragment {
     @BindView(R.id.generic_recycler_view)
     RecyclerView categoriesRecyclerView;
     private CategoryAdapter categoryAdapter;
