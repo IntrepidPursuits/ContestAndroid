17f3395c1b4b068d5e9746f43340276240cdff23
Abass Bayo-Awoyemi <abass.bayo@gmail.com>
2017-02-15 12:57:22 -0500

    remove all references to Parcels from Entry class

--- app/src/main/java/io/intrepid/contest/models/Entry.java
+++ app/src/main/java/io/intrepid/contest/models/Entry.java
@@ -1,12 +1,9 @@
 package io.intrepid.contest.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.util.Date;
 import java.util.UUID;
 
-public class Entry implements Parcelable {
+public class Entry{
     public UUID id;
     public String title;
     public Date createdAt;
@@ -16,32 +13,4 @@ public class Entry implements Parcelable {
     public Entry() {
         id = UUID.randomUUID();
     }
-
-    protected Entry(Parcel in) {
-        title = in.readString();
-        photoUrl = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(photoUrl);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Entry> CREATOR = new Creator<Entry>() {
-        @Override
-        public Entry createFromParcel(Parcel in) {
-            return new Entry(in);
-        }
-
-        @Override
-        public Entry[] newArray(int size) {
-            return new Entry[size];
-        }
-    };
 }
