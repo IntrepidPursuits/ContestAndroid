451928ef9c8818b32b4de77288741fc1dda575a5
Glen Daniels <glen@thoughtcraft.com>
2017-02-22 14:15:21 -0500

    Move shake to BaseActivity :)

--- app/src/main/java/io/intrepid/contest/base/BaseMvpActivity.java
+++ app/src/main/java/io/intrepid/contest/base/BaseMvpActivity.java
@@ -6,14 +6,6 @@ import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import io.intrepid.contest.ContestApplication;
-import io.intrepid.contest.screens.splash.SplashActivity;
-import io.intrepid.contest.utils.ShakeDetector;
-import io.reactivex.Flowable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import timber.log.Timber;
-
 /**
  * Base class for activities that will have some business logic instead of just hosting a fragment.
  * If the activity is only going to act as a container for a fragment, use {@link BaseFragmentActivity}
@@ -22,8 +14,6 @@ import timber.log.Timber;
 public abstract class BaseMvpActivity<T extends BaseContract.Presenter> extends BaseActivity implements BaseContract.View {
 
     protected T presenter;
-    private Flowable<?> shakeFlowable;
-    private Disposable shakeSubscription;
 
     @NonNull
     public abstract T createPresenter(PresenterConfiguration configuration);
@@ -41,7 +31,6 @@ public abstract class BaseMvpActivity<T extends BaseContract.Presenter> extends 
         presenter = createPresenter(configuration);
         onViewCreated(savedInstanceState);
         presenter.onViewCreated();
-        shakeFlowable = ShakeDetector.create(this);
     }
 
     /**
@@ -70,14 +59,6 @@ public abstract class BaseMvpActivity<T extends BaseContract.Presenter> extends 
     public void onStart() {
         super.onStart();
         presenter.bindView(this);
-        shakeSubscription = shakeFlowable.subscribe((Consumer<Object>) o -> {
-            ((ContestApplication) getApplication()).resetState();
-            Intent intent = new Intent(this, SplashActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-            Runtime.getRuntime().exit(0);
-        });
     }
 
     @Override
@@ -85,7 +66,6 @@ public abstract class BaseMvpActivity<T extends BaseContract.Presenter> extends 
     public void onStop() {
         super.onStop();
         presenter.unbindView();
-        shakeSubscription.dispose();
     }
 
     @Override
