39b3d3be8ddd8f69c16fcd63d1a1507d43163458
Abass Bayo-Awoyemi <abass.bayo@gmail.com>
2017-03-13 10:53:58 -0400

    Implement hiding and showing animations

--- app/src/main/java/io/intrepid/contest/screens/contestjudging/expandablerecycler/ScoreViewHolder.java
+++ app/src/main/java/io/intrepid/contest/screens/contestjudging/expandablerecycler/ScoreViewHolder.java
@@ -1,11 +1,15 @@
 package io.intrepid.contest.screens.contestjudging.expandablerecycler;
 
-import android.os.Handler;
+import android.animation.ValueAnimator;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -19,9 +23,7 @@ import io.intrepid.contest.utils.ScoreMapUtil;
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 
-class ScoreViewHolder extends RecyclerView.ViewHolder implements ExpandableViewHolder, RatingBar.OnRatingBarChangeListener {
-    private static final long COLLAPSE_DELAY = 5;
-    private final CategoryScoreListener listener;
+class ScoreViewHolder extends RecyclerView.ViewHolder implements RatingBar.OnRatingBarChangeListener {
     @BindView(R.id.category_item_title)
     TextView categoryTitleField;
     @BindView(R.id.expandable_relative_layout)
@@ -32,14 +34,23 @@ class ScoreViewHolder extends RecyclerView.ViewHolder implements ExpandableViewH
     TextView scoreStatusTextView;
     @BindView(R.id.category_item_description)
     TextView categoryDescriptionField;
-    private boolean reveal;
+    private Animation animationUp;
+    private Animation animationFadeOut;
+    private CategoryScoreListener listener;
     private Score score;
+    private boolean isViewExpanded = false;
+    private ValueAnimator valueAnimator;
+    private int originalHeight;
 
     ScoreViewHolder(ViewGroup parent, CategoryScoreListener listener) {
         super(inflateView(parent));
         this.listener = listener;
         ButterKnife.bind(this, itemView);
         ratingBar.setOnRatingBarChangeListener(this);
+        animationUp = AnimationUtils.loadAnimation(itemView.getContext(), R.anim.slide_up);
+        animationFadeOut = new AlphaAnimation(1.00f, 0.00f);
+        originalHeight = expandableLinearLayout.getHeight();
+        valueAnimator = ValueAnimator.ofInt(originalHeight, originalHeight + originalHeight);
     }
 
     private static View inflateView(ViewGroup parent) {
@@ -48,69 +59,92 @@ class ScoreViewHolder extends RecyclerView.ViewHolder implements ExpandableViewH
     }
 
     void bindScore(Score score) {
-        reveal = (score.getScoreValue() == 0);
-        bindScore(score, reveal);
+        isViewExpanded = (score.getScoreValue() == 0);
+        bindScore(score, isViewExpanded);
     }
 
     private void bindScore(Score score, boolean reveal) {
         this.score = score;
-        this.reveal = reveal;
+        this.isViewExpanded = reveal;
         categoryTitleField.setText(score.getCategoryName());
         categoryDescriptionField.setText(score.getCategoryDescription());
         setRatingPaneVisibility(reveal);
     }
 
     private void setRatingPaneVisibility(boolean reveal) {
-        this.reveal = reveal;
+        this.isViewExpanded = reveal;
         expandableLinearLayout.setVisibility(reveal ? VISIBLE : GONE);
     }
 
-    private void expand() {
-        bindScore(score, true);
-    }
 
-    private void collapse() {
-        bindScore(score, false);
-    }
-
-    private void collapseWithDelay() {
-        //TODO implement animation
-        new Handler().postDelayed(this::collapse, COLLAPSE_DELAY);
-    }
-
-    @Override
-    public boolean isExpanded() {
-        return reveal;
-    }
-
-    @Override
-    public void showTemporaryScoreMessage(@StringRes int message) {
+    private void showScoreMessage(@StringRes int message) {
         scoreStatusTextView.setVisibility(VISIBLE);
         scoreStatusTextView.setText(message);
-        new Handler().postDelayed(() -> scoreStatusTextView.setVisibility(GONE), COLLAPSE_DELAY);
     }
 
-    @Override
-    public void toggle() {
-        if (isExpanded()) {
-            collapseWithDelay();
+    private void doAnimatedToggle() {
+        if (!isViewExpanded) {
+            expand();
+        } else {
+            doAnimatedCollapse();
+        }
+    }
+
+    private void expand() {
+        expandableLinearLayout.setVisibility(View.VISIBLE);
+        expandableLinearLayout.setEnabled(true);
+        isViewExpanded = true;
+    }
+
+
+    private void doAnimatedCollapse() {
+        valueAnimator = ValueAnimator.ofInt(originalHeight + originalHeight, originalHeight);
+        animationFadeOut.setDuration(200);
+        animationUp.setAnimationListener(new AnimationEndListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                expandableLinearLayout.startAnimation(animationFadeOut);
+                expandableLinearLayout.setVisibility(View.GONE);
+            }
+        });
+        expandableLinearLayout.startAnimation(animationUp);
+        doCollapse();
+    }
+
+    private void quickCollapse() {
+        animationFadeOut.setAnimationListener(new AnimationEndListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                expandableLinearLayout.setVisibility(GONE);
+            }
+        });
+        expandableLinearLayout.startAnimation(animationFadeOut);
+        doCollapse();
+    }
+
+    private void doCollapse() {
+        isViewExpanded = false;
+        valueAnimator = ValueAnimator.ofInt(originalHeight + originalHeight, originalHeight);
+        valueAnimator.setDuration(0);
+        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.start();
+    }
+
+    @OnClick(R.id.always_visible_linear_layout)
+    void onClick() {
+        if (isViewExpanded) {
+            quickCollapse();
         } else {
             expand();
         }
     }
 
-    @OnClick(R.id.always_visible_linear_layout)
-    void onClick() {
-        toggle();
-    }
-
     @Override
     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
         int newRating = (int) ratingBar.getRating();
-        showTemporaryScoreMessage(ScoreMapUtil.getRatingTextForScore(newRating));
-        toggle();
-
-        int position = getAdapterPosition();
-        listener.onScoreChanged(position, newRating);
+        showScoreMessage(ScoreMapUtil.getRatingTextForScore(newRating));
+        isViewExpanded = true;
+        doAnimatedToggle();
+        listener.onScoreChanged(getAdapterPosition(), newRating);
     }
 }
