dd56c956107ba28a2ea60fc0c957bed68c522049
Abass Bayo-Awoyemi <abass.bayo@gmail.com>
2017-03-16 16:25:28 -0400

    Init Commit - Update resource files

--- app/src/main/java/io/intrepid/contest/screens/conteststatus/statuswaiting/StatusWaitingFragment.java
+++ app/src/main/java/io/intrepid/contest/screens/conteststatus/statuswaiting/StatusWaitingFragment.java
@@ -3,7 +3,9 @@ package io.intrepid.contest.screens.conteststatus.statuswaiting;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
+import butterknife.BindView;
 import io.intrepid.contest.R;
 import io.intrepid.contest.base.BaseFragment;
 import io.intrepid.contest.base.PresenterConfiguration;
