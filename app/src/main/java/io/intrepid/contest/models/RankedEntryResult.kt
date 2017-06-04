3a19d0571b90676769cf02726c640245b2902d93
Gabi Scardine Silva <gabiscardinesilva@dhcp-18-189-53-34.dyn.MIT.EDU>
2017-06-04 17:42:34 -0400

    Convert RankEntryResult

---
+++ app/src/main/java/io/intrepid/contest/models/RankedEntryResult.kt
@@ -0,0 +1,8 @@
+package io.intrepid.contest.models
+
+class RankedEntryResult {
+    var title: String? = null
+    var photoUrl: String? = null
+    var overallScore: Float = 0f
+    var rank: Int = 0
+}
